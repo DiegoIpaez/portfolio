2a0ec80fffda34eaef5ef2bba58f3f8b50ab63c5
Diegoipaez <diegokpo1313@gmail.com>
2024-01-19 22:52:46 -0300

    added constants and helpers

---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+export * from "./personalInfoContant";
+
+export const SOCIAL_NETWORKS = {
+  LINKEDIN: "https://www.linkedin.com/in/diego-ivan-paez-baa513219/",
+  GITHUB: "https://github.com/DiegoIpaez",
+};
---
+++ src/constants/personalInfoContant.ts
@@ -0,0 +1,17 @@
+const FIRST_NAME = "Diego";
+const LAST_NAME = "Páez";
+
+const FULLNAME = `${FIRST_NAME} ${LAST_NAME}`;
+
+const WORK_START_DATE = new Date("4/1/2022");
+const COUNTRY = "Argentina";
+const LOCATION = "Tucumán";
+
+export const PERSONAL_INFO = {
+  FIRST_NAME,
+  LAST_NAME,
+  FULLNAME,
+  WORK_START_DATE,
+  COUNTRY,
+  LOCATION,
+};
