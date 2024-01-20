4fc3496bd4be3b829aaa5592df7b2716eb88fea9
Diegoipaez <diegokpo1313@gmail.com>
2024-01-19 23:18:08 -0300

    added icons and socialPill

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,4 +3,5 @@ export * from "./personalInfoContant";
 export const SOCIAL_NETWORKS = {
   LINKEDIN: "https://www.linkedin.com/in/diego-ivan-paez-baa513219/",
   GITHUB: "https://github.com/DiegoIpaez",
+  GMAIL: "diegoipaez2000@gmail.com",
 };
