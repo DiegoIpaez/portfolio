983e12482c4b9f86a27e75a4050860c8a6491356
Diegoipaez <diegokpo1313@gmail.com>
2024-01-20 13:18:14 -0300

    added experience section

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,5 @@
 export * from "./personalInfoContant";
+export * from "./experienceContant";
 
 export const SOCIAL_NETWORKS = {
   LINKEDIN: "https://www.linkedin.com/in/diego-ivan-paez-baa513219/",
